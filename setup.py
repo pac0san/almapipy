7eafca05cfcd065c1bec1b52e961c5d41abc448d
pac0san <7056343+pac0san@users.noreply.github.com>
2018-11-17 12:01:40 +0100

    Updating setup.py for libary packaging

--- setup.py
+++ setup.py
@@ -1,39 +1,31 @@
 from setuptools import setup
-#from distutils.core import setup
-try:
-    from pypandoc import convert
+from setuptools import find_packages
 
-    def read_md(f): return convert(f, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-    def read_md(f): return open(f, 'r').read()
-
-
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 setup(
     name="almapipy",
-    packages=['almapipy'],
     version=VERSION,
-    description="Python requests wrapper for the Ex Libris Alma API",
-    license='MIT'
-    long_description=read_md('README.md'),
     author="Steve Pelkey",
     author_email="spelkey@ucdavis.edu",
-    url='https://github.com/UCDavisLibrary/almapipy',
+    description="Python requests wrapper for the Ex Libris Alma API",
+    long_description=long_description,
+#    long_description_content_type="text/markdown",
+    url="https://github.com/UCDavisLibrary/almapipy",
     install_requires=['requests'],
-    keywords='alma exlibris exlibrisgroup api bibliographic'
+    keywords='alma exlibris exlibrisgroup api bibliographic',
+    packages=find_packages(),
     classifiers=[
+        "Programming Language :: Python :: 3"
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-
-        'Programming Language :: Python :: 3.6'
-    ]
+    ],
 )
