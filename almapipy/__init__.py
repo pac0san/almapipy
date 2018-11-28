cd65249f6b092dfc3d51ccf0d5586ce2cab75aaa
pac0san <fco.sanchez@minarnet.es>
2018-11-28 13:11:53 +0100

    Increasing the version to 1.0.1.dev0

--- almapipy/__init__.py
+++ almapipy/__init__.py
@@ -24,7 +24,7 @@ __project_name__ = "almapipy"
 __project_description__ = "Python requests wrapper for the Ex Libris Alma API"
 __project_url__ = "https://github.com/UCDavisLibrary/almapipy"
 __license__ = "MIT License"
-__version__ = "0.1.2.dev3"
+__version__ = "1.0.1.dev0"
 __status__ = "Development"
 
 
